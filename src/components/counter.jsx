8ad9cc0c271e34b3974864c5338208e18cf44e10
Njotu Awasi <51744089+NjotuAwasi@users.noreply.github.com>
2020-08-18 21:35:42 +0100

    added counters.jsx
    modified index.js and counter.jsx

--- src/components/counter.jsx
+++ src/components/counter.jsx
@@ -2,29 +2,50 @@ import React, { Component } from "react";
 
 class Counter extends Component {
   state = {
-    count: 0,
-    tags: [],
+    value: this.props.counter.value,
+    //i: 0,
   };
 
-  renderTags() {
-    if (this.state.tags.length === 0) return <p>There are no tags!</p>;
+  //   constructor() {
+  //     super();
+  //     this.handleIncrement = this.handleIncrement.bind(this);
+  //   }
 
-    return (
-      <ul>
-        {this.state.tags.map((tag) => (
-          <li key={tag}>{tag}</li>
-        ))}
-      </ul>
-    );
-  }
+  handleIncrement = (product) => {
+    console.log(product);
+    this.setState({ value: this.state.value + 1 });
+    //this.setState({ count: this.state.i + 1 });
+  };
 
   render() {
     return (
       <div>
-        {this.state.tags.length === 0 && "Please create a new tag"}
-        {this.renderTags()}
+        <span className={this.getBadgeClasses()}>{this.formatCount()}</span>
+        <button
+          onClick={this.handleIncrement}
+          className="btn btn-secondary btn-sm"
+        >
+          Increment
+        </button>
+        <button
+          onClick={() => this.props.onDelete(this.props.counter.id)}
+          className="btn btn-danger btn-sm m-2"
+        >
+          Delete
+        </button>
       </div>
     );
+  }
+
+  getBadgeClasses() {
+    let classes = "badge m-2 badge-";
+    classes += this.state.value === 0 ? "warning" : "primary";
+    return classes;
+  }
+
+  formatCount() {
+    const { value } = this.state;
+    return value === 0 ? "Zero" : value;
   }
 }
 
